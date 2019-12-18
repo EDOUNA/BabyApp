47caf3da68f0ae29ba279abb8bcdf1c108331215
Erwin Douna <erwin.douna@outlook.com>
2019-12-18 07:55:12 +0100

    Add project files.

---
+++ Models/Nutritions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BabyApp.Models
+{
+    public class Nutritions
+    {
+        public int Id { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+        public Nullable<DateTime> UpdatedTimestamp { get; set; }
+    }
+}

cfc8b657d7df86375f4b4f1dcba7bbb952e3d419
Erwin Douna <erwin.douna@gmail.com>
2019-12-18 08:28:07 +0100

    Adding ORM

---
+++ Models/BabyAppContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BabyApp.Models
+{
+    public class BabyAppContext : DbContext
+    {
+        public DbSet<Nutritions> Nutritions { get; set; }
+    }
+}
